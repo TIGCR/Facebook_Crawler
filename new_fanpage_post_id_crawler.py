9b2f8773f74175be1c33cf624e36f259d39c1569
daysleeper7 <hung.kuochih@gmail.com>
2022-05-06 14:42:31 +0800

    Add files via upload

--- new_fanpage_post_id_crawler.py
+++ new_fanpage_post_id_crawler.py
@@ -1,107 +1,87 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-import time
-import requests, gc, os
-import re
-import math
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  4 22:52:15 2022
+
+@author: Michael
+"""
 import pandas as pd
-import numpy as np
-import datetime
-from selenium.webdriver.common.action_chains import ActionChains #move_to_element
-from selenium.common.exceptions import StaleElementReferenceException #error > element is not attached to the page document
-from selenium.common.exceptions import NoSuchElementException #點完所有按鈕時
+from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
+import time
+import re
+import os
+import logging
+from datetime import datetime,timedelta,date
+import json
 import random
 
-def log_in(email,password):
-    '''
-    輸入Facebook的email和密碼，如:
-    email='12345@gmail.com'
-    passowrd='1234567'
-    '''
+#取得登入cookie
+def get_fb_cookie(email:str, password:str): 
+    chromeOptions = webdriver.ChromeOptions()
+    chromeOptions.add_argument('--headless')
+    driver = webdriver.Chrome(options=chromeOptions)
     driver.get('https://www.facebook.com/')
-    input_1 = driver.find_element_by_css_selector('#email')
-    input_2 = driver.find_element_by_css_selector("input[type='password']")
-
-    input_1.send_keys('email')
-    input_2.send_keys('password')
+    driver.find_element_by_css_selector('#email').send_keys(email)
+    driver.find_element_by_css_selector("input[type='password']").send_keys(password)
     driver.find_element_by_css_selector("button[name='login']").click()
-    time.sleep(1)
-def get_link(i):
-    Link_list=i.find_all('li',{'class':'h676nmdw oi9244e8 q9uorilb'})
-    Link=[]
-    for i in Link_list:
-        try:
-            t=i.find('a')['href']
-            Link.append(t)
-        except:
-            pass
-    return Link
+    time.sleep(2)
+    cookies = driver.get_cookies()
+    s = requests.Session()
+    #將cookies放入session中
+    for cookie in cookies:
+        s.cookies.set(cookie['name'], cookie['value'])
+    driver.quit()
+    return s
 def get_time(x):
-    time_list=x.find_all('a',{'class':'oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gmql0nx0 gpro0wi8 b1v8xokw'})
+    time_list=x.find_all('abbr')
     Time_list=[]
     for i in time_list:
         try:
             try:
-                Time = i['aria-label']
-                Time = datetime.datetime.strptime(Time,  '%Y年%m月%d日')
-                Time = Time.strftime("%Y-%m-%d")
+                Time = i.text
+                Time=re.search(r'(\d{4}年\d{1,2}月\d{1,2}日)',Time).group()
+                Time = datetime.strptime(Time,  '%Y年%m月%d日')
+                Time = Time.strftime("%Y-%m-%d").date()
             except:
-                Time=i['aria-label']
+                Time=i.text
                 Time='2022年'+Time
-                Time = datetime.datetime.strptime(Time,  '%Y年%m月%d日')
+                Time=re.search(r'(\d{4}年\d{1,2}月\d{1,2}日)',Time).group()
+                Time = datetime.strptime(Time,  '%Y年%m月%d日')
                 Time = Time.strftime("%Y-%m-%d")
-                Time =datetime.datetime.strptime(Time,'%Y-%m-%d')
+                Time =datetime.strptime(Time,'%Y-%m-%d').date()
         except:
-            Time = 'Not Post'
+            Time = date.today()
         Time_list.append(Time)
     return Time_list
-def get_POST_ID(x):
-    POST_ID_LIST=[]
-    for i in x:
-        postid=i.split('posts/')[1][0:15]
-        POST_ID_LIST.append(postid)
-    return POST_ID_LIST
-def crawl_postid (url,until_date):
-    '''
-    url:欲爬的網址
-    until_date:爬到幾月幾號(只能爬抽過一個月)，格式:'2022-01-01'
-    '''
-    driver.get(url)
-    until_date=datetime.datetime.strptime(until_date,'%Y-%m-%d')
-    date=datetime.datetime.now()
-    scroll_top=0 #滾輪
-    time.sleep(5)
-    while date > until_date:
-        for i in range(0,10):
-            scroll_top+=500
-            js=f"var action=document.documentElement.scrollTop={scroll_top}"
-            driver.execute_script(js)
-            time.sleep(1)
-        soup = BeautifulSoup(driver.page_source)
-        LINKS=get_link(soup)
-        TIMES=get_time(soup)
-        date=TIMES[-1]
-        if date=='Not Post':
-            date=datetime.datetime.now()
-        #else:
-            #date=datetime.datetime.strptime(date,  '%Y-%m-%d-')
-        time.sleep(random.choice([i for i in range(3, 8)]))
-    LINK=get_link(soup)[1:]
-    POST_IDS=get_POST_ID(LINK)
-    return POST_IDS
-def get_page_id(x):
+def get_post_id(x):
+    t1=x.find_all('a',{'class','_5ayv _qdx'})
+    id_list=[]
+    for i in t1:
+        id_list.append(re.search(r'post_id.(\d{1,20})',i['href']).group().replace('post_id.',''))
+    
+    return id_list
+def get_next_page(x):
+    url3=0
+    a=x.find_all('a')
+    while type(url3)==int:
+        try:
+            if a[url3].text=='查看更多動態':
+                url3='https://mbasic.facebook.com/'+a[url3]['href']
+            else:
+                url3+=1
+        except:
+            url3+=1
+            pass
+    return url3
+def get_PAGEID(x):
     headers = {'accept': 'text/html',
                    'sec-fetch-user': '?1'}
     rs = requests.Session()
     content_df = [] # post
     feedback_df = [] # reactions
     timeline_cursor = ''
-    max_date =  datetime.datetime.now().strftime('%Y-%m-%d')
+    max_date =  datetime.now().strftime('%Y-%m-%d')
     break_times = 0
     pageurl=x
         # Get PageID
@@ -111,19 +91,52 @@ def get_page_id(x):
     except:
         pageid = re.findall('delegate_page":\{"id":"(.*?)"\},', resp.text)[0]
     return pageid
-driver = webdriver.Chrome('chromedriver')
-
+def crawl_post_id(email,password,url,until_date):
+    until_date=datetime.strptime(until_date,'%Y-%m-%d').date()
+    s=get_fb_cookie(email=email,password=password)
+    headers={'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+ 'accept-encoding': 'gzip, deflate, br',
+ 'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+ 'cache-control': 'max-age=0',
+ 'referer': 'https://mbasic.facebook.com/mohw.gov.tw/?refid=52&__tn__=C-R',
+ 'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+ 'sec-ch-ua-mobile': '?0',
+ 'sec-ch-ua-platform': '"Windows"',
+ 'sec-fetch-dest': 'document',
+ 'sec-fetch-mode': 'navigate',
+ 'sec-fetch-site': 'same-origin',
+ 'sec-fetch-user': '?1',
+ 'upgrade-insecure-requests': '1',
+ 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'}
+    dates=date.today()
+    fan_page_name=re.search(r'.com/(\S{1,100})',url).group().replace('.com/','')
+    response=s.get(f'https://mbasic.facebook.com/{fan_page_name}',headers=headers)
+    soup = BeautifulSoup(response.text, features="lxml")
+    url2='https://mbasic.facebook.com'+soup.find('div',{'class':'bk'}).find_all('a')[-3]['href']
+    print(f'粉絲專業名稱{fan_page_name}')
+    time.sleep(3)
+    post_id_list=[]
+    while dates > until_date:
+        response=s.get(url2)
+        soup = BeautifulSoup(response.text, features="lxml")
+        ids=get_post_id(soup)
+        for i in ids:
+            post_id_list.append(i)
+        TIMES=get_time(soup)
+        dates=TIMES[-1]
+        url2=get_next_page(soup)
+        print(f'爬到:{dates}\n 已爬：{len(post_id_list)}貼文')
+        #else:
+            #date=datetime.datetime.strptime(date,  '%Y-%m-%d-')
+        time.sleep(random.choice([i for i in range(3, 8)]))
+        
+    #soups = BeautifulSoup(driver.page_source)
+    return post_id_list
 email=''
 password=''
-
-log_in(email,password)
-# URL of FB Fan Page e.g. https://www.facebook.com/joebiden  https://www.facebook.com/WithGaLoveTaiwan
-url='https://www.facebook.com/'
-
-driver.get(url)
-# until_date 大約爬貼文id的最早日期 
-post_IDs=crawl_postid (url,until_date='2022-02-01')
+url='URL of FB Fan page'
+post_IDs=crawl_post_id(email=email,password=password,url=url,until_date='2022-04-25')
 df=pd.DataFrame(post_IDs,columns=['POSTID'])
-df['PAGEID']=get_page_id(url)
-# 將爬文的dataframe存成一個excel檔
-df.to_excel('POSTID&PAGEID.xlsx')
+df=df.drop_duplicates()
+df['PAGEID']=get_PAGEID(url)
+df.to_excel('POSTID&PAGEID.xlsx',index=False)
